5b119a7c4ac452327f94518a4a3a95645d5e91ec
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-25 00:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function manipulateData(inputArray) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  });
+  
+  promise.then(array => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const filteredArray = array.filter(num => num % 2 === 0);
+        resolve(filteredArray);
+      }, 1000);
+    });
+  })
+  .then(filteredArray => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const multipliedArray = filteredArray.map(num => num * 2);
+        resolve(multipliedArray);
+      }, 2000);
+    });
+  })
+  .then(finalArray => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = finalArray.join(", ");
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+// Example usage
+manipulateData([1, 2, 3, 4]);
